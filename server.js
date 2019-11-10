c9f49f34cc9f48eacb1fc7654bb13f1ea9f7cf86
linhletd <42126424+linhletd@users.noreply.github.com>
2019-11-10 15:01:48 +0700

    Update server.js

--- server.js
+++ server.js
@@ -4,20 +4,22 @@ var express = require('express');
 var cors = require('cors');
 
 // require and use "multer"...
-
+var multer = require('multer');
 var app = express();
-
+app.use(multer({ dest: __dirname}).single('upfile'));
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
-     res.sendFile(process.cwd() + '/views/index.html');
+     res.sendFile(__dirname + '/views/index.html');
   });
 
 app.get('/hello', function(req, res){
   res.json({greetings: "Hello, API"});
 });
-
+app.post('/api/fileanalyse',(req, res) => {
+  res.json({name: req.file.originalname, type: req.file.mimetype, size: req.file.size});
+})
 app.listen(process.env.PORT || 3000, function () {
   console.log('Node.js listening ...');
 });
